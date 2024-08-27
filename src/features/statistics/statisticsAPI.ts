6dd506135cd664480ac5bdcfe6a15643f682999f
alemayehu.dev <alemayehu.dev@gmail.com>
2024-08-27 05:20:31 -0700

    implement the stat api's

--- src/features/statistics/statisticsAPI.ts
+++ src/features/statistics/statisticsAPI.ts
@@ -1,21 +1,28 @@
 import axios from "axios";
 
-export const fetchStatisticsData = () => {
-  return axios.get("/api/stats");
+const API_URL = "https://song-manager-api.onrender.com/api/stats";
+
+export const fetchStatisticsData = async () => {
+   const response = await axios.get(API_URL);
+   return response.data;
 };
 
-export const fetchGenres = () => {
-  return axios.get("/api/stats/genre");
+export const fetchGenres = async () => {
+  const response = await axios.get(`${API_URL}/genre`);
+  return response.data;
 };
 
-export const fetchArtists = () => {
-  return axios.get("/api/stats/artist");
+export const fetchArtists = async () => {
+  const response = await axios.get(`${API_URL}/artist`);
+  return response.data;
 };
 
-export const fetchAlbums = () => {
-  return axios.get("/api/stats/album");
+export const fetchAlbums = async () => {
+  const response = await axios.get(`${API_URL}/artist`);
+  return response.data;
 };
 
-export const fetchSongsInAlbum = () => {
-  return axios.get("/api/stats/songs-in-album");
+export const fetchSongsInAlbum = async () => {
+  const response = await axios.get(`${API_URL}/songs-in-album`);
+  return response.data;
 };
