eae3cd04c1cf841a03501fda1a59e87380e53e3f
alemayehu.dev <alemayehu.dev@gmail.com>
2024-08-27 05:20:46 -0700

    build the saga

--- src/features/statistics/statisticsSaga.ts
+++ src/features/statistics/statisticsSaga.ts
@@ -1,46 +1,50 @@
-import { all, call, put, takeEvery } from "redux-saga/effects"; // Fix missing 'all' import
-import { fetchStatistics } from "./statisticsSlice"; // Fix import issue
+import { all, call, put, takeEvery } from "redux-saga/effects";
+import { fetchStatistics } from "./statisticsSlice";
 import {
   fetchStatisticsData,
   fetchGenres,
   fetchArtists,
   fetchAlbums,
-  fetchSongsInAlbum,
+  // fetchSongsInAlbum, // Removed if not used
 } from "./statisticsAPI";
 
-// Define the return type for fetchStatisticsSaga
 function* fetchStatisticsSaga(): Generator<any, void, any> {
- try {
-   // Use all to run effects in parallel
-   const [
-     totalResponse,
-     genresResponse,
-     artistsResponse,
-     albumsResponse,
-     songsInAlbumResponse,
-   ] = yield all([
-     call(fetchStatisticsData),
-     call(fetchGenres),
-     call(fetchArtists),
-     call(fetchAlbums),
-     call(fetchSongsInAlbum),
-   ]);
+  try {
+    const [
+      totalResponse,
+      genresResponse,
+      artistsResponse,
+      albumsResponse,
+      // songsInAlbumResponse,
+    ] = yield all([
+      call(fetchStatisticsData),
+      call(fetchGenres),
+      call(fetchArtists),
+      call(fetchAlbums),
+      // call(fetchSongsInAlbum),
+    ]);
 
-   yield put(
-     fetchStatistics.fulfilled({
-       totalSongs: totalResponse.data.totalSongs,
-       totalArtists: totalResponse.data.totalArtists,
-       totalAlbums: totalResponse.data.totalAlbums,
-       totalGenres: totalResponse.data.totalGenres,
-       songsByGenre: genresResponse.data,
-       songsByArtist: artistsResponse.data,
-       songsByAlbum: albumsResponse.data,
-       songsInAlbum: songsInAlbumResponse.data,
-     })
-   );
- } catch (error) {
-   console.error("Failed to fetch statistics:", error);
- }
+    yield put(
+      fetchStatistics.fulfilled(
+        {
+          totalSongs: totalResponse.data.totalSongs,
+          totalArtists: totalResponse.data.totalArtists,
+          totalAlbums: totalResponse.data.totalAlbums,
+          totalGenres: totalResponse.data.totalGenres,
+          songsByGenre: genresResponse.data,
+          songsByArtist: artistsResponse.data,
+          songsByAlbum: albumsResponse.data,
+          // songsInAlbum: songsInAlbumResponse.data,
+        },
+        "uniqueRequestId"
+      )
+    );
+  } catch (error) {
+    const errorMessage = (error as Error).message || "Unknown error";
+    yield put(
+      fetchStatistics.rejected({ error: errorMessage }, "uniqueRequestId")
+    );
+  }
 }
 
 export function* statisticsSaga() {
