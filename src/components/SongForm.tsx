c69a3660de44cd9a22d9bd5e03c880ac1fbbd802
alemayehu.dev <alemayehu.dev@gmail.com>
2024-08-29 00:51:35 -0700

    change the theme color

--- src/components/SongForm.tsx
+++ src/components/SongForm.tsx
@@ -1,4 +1,3 @@
-// src/components/SongForm.tsx
 import React from "react";
 import {
   TextField,
@@ -6,6 +5,8 @@ import {
   DialogContent,
   DialogActions,
   MenuItem,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import { GENRES } from "../constants";
 import { Song } from "../features/songs/types";
@@ -22,60 +23,159 @@ const SongForm: React.FC<SongFormProps> = ({
   setFormValues,
   handleSubmit,
   handleClose,
-}) => (
-  <React.Fragment>
-    <DialogContent>
-      <TextField
-        margin="dense"
-        label="Title"
-        fullWidth
-        value={formValues.title || ""}
-        onChange={(e) =>
-          setFormValues({ ...formValues, title: e.target.value })
-        }
-      />
-      <TextField
-        margin="dense"
-        label="Artist"
-        fullWidth
-        value={formValues.artist || ""}
-        onChange={(e) =>
-          setFormValues({ ...formValues, artist: e.target.value })
-        }
-      />
-      <TextField
-        margin="dense"
-        label="Album"
-        fullWidth
-        value={formValues.album || ""}
-        onChange={(e) =>
-          setFormValues({ ...formValues, album: e.target.value })
-        }
-      />
-      <TextField
-        margin="dense"
-        label="Genre"
-        fullWidth
-        value={formValues.genre || ""}
-        onChange={(e) =>
-          setFormValues({ ...formValues, genre: e.target.value })
-        }
-        select
-      >
-        {GENRES.map((genre) => (
-          <MenuItem key={genre} value={genre}>
-            {genre}
-          </MenuItem>
-        ))}
-      </TextField>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleClose}>Cancel</Button>
-      <Button onClick={handleSubmit}>
-        {formValues.id ? "Update" : "Add"}
-      </Button>
-    </DialogActions>
-  </React.Fragment>
-);
+}) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // Optional: adjust layout based on screen size
+
+  return (
+    <React.Fragment>
+      <DialogContent sx={{ bgcolor: theme.palette.background.paper }}>
+        <TextField
+          margin="dense"
+          label="Title"
+          fullWidth
+          value={formValues.title || ""}
+          onChange={(e) =>
+            setFormValues({ ...formValues, title: e.target.value })
+          }
+          sx={{
+            mb: 2,
+            "& .MuiInputLabel-root": {
+              color: theme.palette.text.secondary,
+            },
+            "& .MuiInputBase-input": {
+              color: theme.palette.text.primary,
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: theme.palette.text.secondary,
+              },
+              "&:hover fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          label="Artist"
+          fullWidth
+          value={formValues.artist || ""}
+          onChange={(e) =>
+            setFormValues({ ...formValues, artist: e.target.value })
+          }
+          sx={{
+            mb: 2,
+            "& .MuiInputLabel-root": {
+              color: theme.palette.text.secondary,
+            },
+            "& .MuiInputBase-input": {
+              color: theme.palette.text.primary,
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: theme.palette.text.secondary,
+              },
+              "&:hover fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          label="Album"
+          fullWidth
+          value={formValues.album || ""}
+          onChange={(e) =>
+            setFormValues({ ...formValues, album: e.target.value })
+          }
+          sx={{
+            mb: 2,
+            "& .MuiInputLabel-root": {
+              color: theme.palette.text.secondary,
+            },
+            "& .MuiInputBase-input": {
+              color: theme.palette.text.primary,
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: theme.palette.text.secondary,
+              },
+              "&:hover fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          label="Genre"
+          fullWidth
+          value={formValues.genre || ""}
+          onChange={(e) =>
+            setFormValues({ ...formValues, genre: e.target.value })
+          }
+          select
+          sx={{
+            mb: 2,
+            "& .MuiInputLabel-root": {
+              color: theme.palette.text.secondary,
+            },
+            "& .MuiInputBase-input": {
+              color: theme.palette.text.primary,
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: theme.palette.text.secondary,
+              },
+              "&:hover fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: theme.palette.primary.main,
+              },
+            },
+          }}
+        >
+          {GENRES.map((genre) => (
+            <MenuItem key={genre} value={genre}>
+              {genre}
+            </MenuItem>
+          ))}
+        </TextField>
+      </DialogContent>
+      <DialogActions sx={{ bgcolor: theme.palette.background.default }}>
+        <Button
+          onClick={handleClose}
+          sx={{ color: theme.palette.text.secondary }}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleSubmit}
+          sx={{
+            color: theme.palette.primary.contrastText,
+            backgroundColor: theme.palette.primary.main,
+            "&:hover": {
+              backgroundColor: theme.palette.primary.dark,
+            },
+          }}
+        >
+          {formValues.id ? "Update" : "Add"}
+        </Button>
+      </DialogActions>
+    </React.Fragment>
+  );
+};
 
 export default SongForm;
