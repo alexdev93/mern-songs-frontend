11021ad187f83fcde729ab755a6d15e406ac278e
alemayehu.dev <alemayehu.dev@gmail.com>
2024-08-29 00:11:13 -0700

    use theme

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2", // Customize your primary color
+    },
+    secondary: {
+      main: "#dc004e", // Customize your secondary color
+    },
+    text: {
+      primary: "#fff", // Primary text color
+      secondary: "#b0b0b0", // Secondary text color
+    },
+    background: {
+      default: "#0f0f0f", // Background color for dark mode
+      paper: "#383838", // Paper color for dark mode
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+  },
+});
+
+export default theme;
